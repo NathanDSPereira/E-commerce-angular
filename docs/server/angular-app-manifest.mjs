18ffa339219e070246891cfc155965499b34c568
= <=>
2025-07-01 16:37:15 -0300

    configuracao para o GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 4183, hash: 'ad2c8b6272abfe88eb71a8325fd46932da0a1822a0682691ce5e542d7e5d6ad0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: '021db9a8d876c008d25cbbbc1981607bd861df19cf133460b95710b67d6d7b3a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-DQBQA3A4.css': {size: 11599, hash: 'HIxOrtF5L5Q', text: () => import('./assets-chunks/styles-DQBQA3A4_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5564, hash: '1e076303aced8ff0b269092b0bdac6bfd7e2edb3a9ffa0de46e7dbe466f9ee29', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: 'be119db7c868a8422afe9f1178371fd1a7c28fe18f4ca5cbd294c0cc5c037efd', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-FLUQKAZC.css': {size: 14700, hash: 'mWsJyqsoEIw', text: () => import('./assets-chunks/styles-FLUQKAZC_css.mjs').then(m => m.default)}
   },
 };
