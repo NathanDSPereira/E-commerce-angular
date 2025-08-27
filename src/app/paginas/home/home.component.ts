62a483229596c5960122f1fa61c05c67fcede65a
NathanDSPereira <nathan6780p@gmail.com>
2025-08-27 05:08:17 -0300

    criacao de outros componentes e melhora nos existentes

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
 import { CommonModule } from '@angular/common';
 import { ListaProdutosComponent } from '../../componentes/produtos/lista-produtos.component';
+import { FeedbacksComponent } from '../../componentes/feedbacks/feedbacks.component';
+import { RodapeComponent } from '../../componentes/rodape/rodape.component';
 
 @Component({
   selector: 'home-app',
-  imports: [FormsModule, CabecalhoComponent, CommonModule, ListaProdutosComponent],
+  imports: [FormsModule, CabecalhoComponent, CommonModule, ListaProdutosComponent, FeedbacksComponent, RodapeComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
