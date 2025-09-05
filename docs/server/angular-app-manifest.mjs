ccb4a755fe4320fe62049a1d29ff0e798ecfc4a7
NathanDSPereira <nathan6780p@gmail.com>
2025-09-05 05:53:28 -0300

    build da pasta docs para carregar as imagens

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/E-commerce-angular',
+  baseHref: '/E-commerce-angular/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 6379, hash: 'a31f237dc762150f25bb0b7a4db1a5845d4ac7028dfefee69926a4ffd05d95e2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1026, hash: '8c582bfed931405e622771c846daa0b946331ad171d4a4b4fa6f4dc0c91822da', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-6REPDTVZ.css': {size: 20448, hash: '+144az7DywQ', text: () => import('./assets-chunks/styles-6REPDTVZ_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7404, hash: 'a2fa5900da25b29534a6726f7f9742d9396bbd8cfd3d38ad9833c6beaa3f2d9f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: '6d7855d3e8987ba08c45f70520191039f86342237771caef4db3c8d6b77199e7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-BTKKG5V7.css': {size: 23265, hash: '+fNGMeD2/aU', text: () => import('./assets-chunks/styles-BTKKG5V7_css.mjs').then(m => m.default)}
   },
 };
