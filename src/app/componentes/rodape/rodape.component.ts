1d499eaccfa8490dccf9aec3dae0da045dc2417b
NathanDSPereira <nathan6780p@gmail.com>
2025-09-10 05:25:56 -0300

    atualização do visual dos produtos card

--- src/app/componentes/rodape/rodape.component.ts
+++ src/app/componentes/rodape/rodape.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rodape',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './rodape.component.html',
   styleUrl: './rodape.component.css'
 })
 export class RodapeComponent {
-
+  boleano: boolean = true;
 }
