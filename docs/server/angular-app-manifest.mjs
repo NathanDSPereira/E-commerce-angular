5359f5907f4e88f47ac3342632ba3b4652b2f379
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-09-15 08:41:53 -0300

    commit para realizar o pull do remoto

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7490, hash: '93249abd43d6cccc52e9dd0452c98176399287c38c22c5125464353d4da83cc8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: '1bb4cefd591d1d2a6a796758a0638a4729499c4e6e05dd3c3de64ab4f25e09af', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-A4OOXUVA.css': {size: 23798, hash: 'ldzuVhZy3/w', text: () => import('./assets-chunks/styles-A4OOXUVA_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7534, hash: '84a295a011213e85d7c34ee219ed0798006d13517c195e4eb6c2b984058889a1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: '3730f57f7c43cc482e0b89e7e577b3f2d434762436df0f37b6a453b8f10f325b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-PWPWXYMS.css': {size: 25317, hash: '6JVHlOK+/Ag', text: () => import('./assets-chunks/styles-PWPWXYMS_css.mjs').then(m => m.default)}
   },
 };
