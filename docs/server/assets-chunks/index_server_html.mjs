4f791ad910dc1adae97e05ba9cb4e2501e821eca
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-09-19 15:21:21 -0300

    pull do remoto

--- docs/server/assets-chunks/index_server_html.mjs
+++ docs/server/assets-chunks/index_server_html.mjs
@@ -6,26 +6,10 @@ export default `<!doctype html>
   <base href="/E-commerce-angular/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<link rel="stylesheet" href="styles-PWPWXYMS.css"></head>
+<link rel="stylesheet" href="styles-5CJMXKUH.css"></head>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
   <home-app></home-app>
-<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-V2P5WIBR.js" type="module"></script></body>
-=======
-<link rel="stylesheet" href="styles-2NB5Q6EE.css"></head>
-<body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
-</script>
-  <home-app></home-app>
-<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-PRGIQ4VE.js" type="module"></script></body>
->>>>>>> bdeddcdc624d26995a763e9662771ef25b112f69
-=======
-<link rel="stylesheet" href="styles-QKBRO4NM.css"></head>
-<body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
-</script>
-  <home-app></home-app>
-<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-J4MRKZNH.js" type="module"></script></body>
->>>>>>> d19f206cd1f24354cdad0c32ccb5cd5e254cb932
+<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-VYJXJIUQ.js" type="module"></script></body>
 </html>
 `;
--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,10 +1,2 @@
 import './polyfills.server.mjs';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-ZGLIAT4E.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
-=======
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-G5PCW6DY.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
->>>>>>> bdeddcdc624d26995a763e9662771ef25b112f69
-=======
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-KGXIRQLT.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
->>>>>>> d19f206cd1f24354cdad0c32ccb5cd5e254cb932
+import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-ARESMBT6.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
