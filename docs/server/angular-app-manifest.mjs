4ceb8cd27636df618cd282f9d4c52b0530b63d57
NathanDSPereira <nathan6780p@gmail.com>
2025-09-24 05:16:30 -0300

    corrindo o erro page not found github pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7646, hash: '7b978f857e25f2d9d460a2cfdf9dbe15ab538b465fb7ba8a7c23453ad4ec6df2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: 'fe674e7db099cac25a9f2e61d16e7aea51a71ed9f4af714b735a553a3c9d44db', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-IKHWC3SB.css': {size: 25824, hash: 'eVDtrDnhxlo', text: () => import('./assets-chunks/styles-IKHWC3SB_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7646, hash: '6a56f41d8c2d21a2d0fc05cc2902414010ddd8025a6932eb4855c68976597b51', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: 'aa300bb8e511227df33eb4946d18a99ed644f2ff28d0e7fa3ef9f5ac21d9919b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-QA6XE2IY.css': {size: 26114, hash: 'Snp0+FjN7pU', text: () => import('./assets-chunks/styles-QA6XE2IY_css.mjs').then(m => m.default)}
   },
 };
