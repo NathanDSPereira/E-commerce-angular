a036df12fb5bc9bbab59877c62f5070b2eddbd34
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-09-25 08:11:47 -0300

    pull do remoto

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,18 +1,2 @@
 import './polyfills.server.mjs';
-<<<<<<< HEAD
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-ARESMBT6.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
-=======
-<<<<<<< HEAD
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-UCM5N3L4.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-ZGLIAT4E.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
-=======
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-G5PCW6DY.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
->>>>>>> bdeddcdc624d26995a763e9662771ef25b112f69
-=======
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-KGXIRQLT.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
->>>>>>> d19f206cd1f24354cdad0c32ccb5cd5e254cb932
->>>>>>> d77184f83ed6c9b8e8cc151d9f1a2e3554fcabfa
->>>>>>> 78a4c35a1758e06f2035e68ba0839f99da9589fd
+import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-AXYXK6NK.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
