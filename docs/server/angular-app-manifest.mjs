4011b7c98c9bc4eb83d6430c726704005a78afeb
NathanDSPereira <nathan6780p@gmail.com>
2025-09-25 05:41:29 -0300

    configurando a lógica para filtrar por categoria

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7646, hash: '6a56f41d8c2d21a2d0fc05cc2902414010ddd8025a6932eb4855c68976597b51', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: 'aa300bb8e511227df33eb4946d18a99ed644f2ff28d0e7fa3ef9f5ac21d9919b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7646, hash: '8a294820e2e5f1c8f80c1b6b22b51ff805e552fcc69ab2d8489ef27538764b12', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: '5a718e82931fe400bca45ae3518d79718c73ecfe72749c50dd4eb6ef2d1b11be', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QA6XE2IY.css': {size: 26114, hash: 'Snp0+FjN7pU', text: () => import('./assets-chunks/styles-QA6XE2IY_css.mjs').then(m => m.default)}
   },
 };
