030bdde2ae40a4d81afafe40d2dceff6ce7b36d4
NathanDSPereira <nathan6780p@gmail.com>
2025-09-26 05:28:46 -0300

    ng deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7682, hash: '772a56dc6c42917865992c8c25111911d1a58d1cada2dabcbd7e31167e37e199', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: '336b7a2ae4864337330967b7d6ed291ab6e0f3e445c25233e3d60ed856e1db6a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-YW3WWD3P.css': {size: 26291, hash: 'S25YfwoBnLs', text: () => import('./assets-chunks/styles-YW3WWD3P_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7682, hash: '9a306795c9c77252b8b00ebe75e8438bd286a24f130d7f0d7d2a3d16ed0762f2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: 'f34b6d7184e974f7bb73f79152deeb9904778beabb2f5d340bfa719d82fe4e01', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-IO4VUHLB.css': {size: 26271, hash: 'qkNO3lHeCj4', text: () => import('./assets-chunks/styles-IO4VUHLB_css.mjs').then(m => m.default)}
   },
 };
