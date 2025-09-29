4b111e8eb9153691e4189c78d17451fed5c979f7
NathanDSPereira <nathan6780p@gmail.com>
2025-09-29 05:31:26 -0300

    ng deploy das mudanças

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/E-commerce-angular',
+  baseHref: '/E-commerce-angular/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7681, hash: '44b611ea906f5e41f397d779906048aae889ab0645e38c2fb40e3a7fb2d6175a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1026, hash: 'fb8021fce47e390acab406eeeb5a2be87dc0730e37460d8143ee674a1e6afbe8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-22IRX3SU.css': {size: 26410, hash: '3LGhfYb2gx4', text: () => import('./assets-chunks/styles-22IRX3SU_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7682, hash: 'ca7acc524e03e5062fadfd279f4a9a181ec13c5aea1df87053be79a2a1a5a255', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: '6bd1233f92f33dbe38e3b791b895f05a1975da592eeab782af03f95c3f77348a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-VOP37R66.css': {size: 26447, hash: '3PCJnP+DyBk', text: () => import('./assets-chunks/styles-VOP37R66_css.mjs').then(m => m.default)}
   },
 };
