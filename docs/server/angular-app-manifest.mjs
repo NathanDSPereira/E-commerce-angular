271e8ae84382848e3558821c6000a7f479779768
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-10-01 15:52:03 -0300

    ng deploy das modificações

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7682, hash: '86650caa8bf55e10096d6f63140d06b1eea3fea81b24bc0869bf7f6dfde6490e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: 'bc19f8ec9769ec1607913083010fcdcd3664ba7c98aefa24248389ba1215ef8a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-CURPB4RV.css': {size: 26566, hash: 'f4pafU4yYlM', text: () => import('./assets-chunks/styles-CURPB4RV_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7726, hash: 'fbe6990af411e83ad51a9c4c35f69e6fc68e74d856e21a579fa7c3efe46a0984', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: '64307d1c482be2ba3888bbb68f0cf7a8205c38db0ed9aebee61973420d2b9b8b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-PCSUJY7R.css': {size: 27239, hash: 'lcUT/h7VLxI', text: () => import('./assets-chunks/styles-PCSUJY7R_css.mjs').then(m => m.default)}
   },
 };
