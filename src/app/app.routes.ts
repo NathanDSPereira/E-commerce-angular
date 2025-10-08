7c99fd87b7b2aaf7dc70831aab7abb2bfae4733b
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-10-08 15:06:08 -0300

    inicio da criação do componente de perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomePageComponent } from './componentes/homePage/homepage.component';
 import { LoginComponent } from './paginas/login/login.component';
 import { LayoutComponent } from './componentes/layout/layout.component';
 import { RegistrarComponent } from './paginas/registrar/registrar.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
@@ -16,7 +18,8 @@ export const routes: Routes = [
         children: [
             {path: '', redirectTo: 'home', pathMatch: 'full'},
             {path: 'home', component: HomePageComponent},
-            {path: 'produtos', component: ListaProdutosComponent}
+            {path: 'produtos', component: ListaProdutosComponent},
+            {path: 'perfil', component: PerfilComponent, canActivate: [authGuard]}
         ]
     }
 ]
