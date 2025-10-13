dd2c7b36459936c4e56496318783abe64937d005
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-10-13 07:55:34 -0300

    ajustes na tela de home e inicio da tela de registrar-se

--- src/app/paginas/registrar/registrar.component.ts
+++ src/app/paginas/registrar/registrar.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { NOMEM } from 'dns';
 
 
 @Component({
   selector: 'app-registrar',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, FormsModule],
   templateUrl: './registrar.component.html',
   styleUrl: './registrar.component.css'
 })
 export class RegistrarComponent {
+  contatoForm: FormGroup;
 
+  constructor(private route: Router) {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl(''),
+      email: new FormControl(''),
+      telefone: new FormControl(''),
+      senha: new FormControl('')
+    })
+  }
+
+  registrar(): void {
+
+    this.route.navigate(['/login'])
+  }
 }
