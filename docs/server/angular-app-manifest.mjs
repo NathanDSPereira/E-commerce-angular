362efc6cf3c2e2b59f0a13086e991e91b27a6865
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2025-10-22 12:22:31 -0300

    ng deploy das modificações

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 7854, hash: '57bcaf2b9752f67f36faf3e25ed632cd15eb64b745ff06d08cce3d9d00292966', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1027, hash: 'bfa3ac07906926de82188b385e63a0328e552024ca061317fa56bca29d78c4ce', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7854, hash: '1c7281226e2ab0275eea3099b85330c6e896007297695faf2379cdcfbbd9f974', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1027, hash: 'b7a73a865ec3ebb468d3874bac69213d983be2ceb7930e409e5083e0a659ee9c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-RWJLOLRM.css': {size: 29893, hash: 'GfOOrI57HYo', text: () => import('./assets-chunks/styles-RWJLOLRM_css.mjs').then(m => m.default)}
   },
 };
